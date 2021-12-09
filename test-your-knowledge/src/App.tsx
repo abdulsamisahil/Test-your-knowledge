905d07f8d09c85577ff8d4ffdb6de67a71515451
Abdul Sami <sami.kdr@live.se>
2021-12-09 14:31:18 +0100

    project started

---
+++ test-your-knowledge/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Testing your knowledge</h1>
+     <h2>Start answering the quiz questions</h2>
+    </div>
+  );
+}
+export default App;
